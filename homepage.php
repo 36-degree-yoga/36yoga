152b5bfa1f4c7bad0fae2af8d4c7601a2801639f
YujuKuo <yujuwk02@gmail.com>
2020-12-05 11:55:32 +0800

    軟 登入註冊驗證碼 首頁手機版動畫

--- homepage.php
+++ homepage.php
@@ -721,36 +721,40 @@
                     <div class="index_hotitem_wrap i_hot_w01">
                         <img src="./img/index/index_11_inner01.jpg" alt="" class="index_hotitem hotitem_01">
                         <img src="./img/index/index_11_01.jpg" alt="" class="index_back11">
+
+                        <div class="index_hot_ppwrap hot_ppwrap01 d-flex flex-column text-center">
+                            <a class='index_hot_a' href="">
+                                <h6>好瘦瑜珈磚</h6>
+                            </a>
+                            <p>NT$. 1000</p>
+                        </div>
                     </div>
-                    <div class="index_hot_ppwrap hot_ppwrap01 d-flex flex-column text-center">
-                        <a class='index_hot_a' href="">
-                            <h6>好瘦瑜珈磚</h6>
-                        </a>
-                        <p>NT$. 1000</p>
-                    </div>
+                    
                     
       
                     <div class="index_hotitem_wrap i_hot_w02">         
                         <img src="./img/index/index_11_inner02.jpg" alt="" class="index_hotitem hotitem_02">  
-                        <img src="./img/index/index_11_02.jpg" alt="" class="index_back11_02">      
+                        <img src="./img/index/index_11_02.jpg" alt="" class="index_back11_02">   
+                        
+                        <div class="index_hot_ppwrap hot_ppwrap02 d-flex flex-column text-center">
+                            <a class='index_hot_a' href="">
+                                <h6>好瘦瑜珈磚</h6>
+                            </a>
+                            <p>NT$. 1000</p>
+                        </div>
                     </div>
-                    <div class="index_hot_ppwrap hot_ppwrap02 d-flex flex-column text-center">
-                        <a class='index_hot_a' href="">
-                            <h6>好瘦瑜珈磚</h6>
-                        </a>
-                        <p>NT$. 1000</p>
-                    </div>
-
+                    
 
                     <div class="index_hotitem_wrap i_hot_w03">     
                         <img src="./img/index/index_11_inner03.jpg" alt="" class="index_hotitem hotitem_03">  
                         <img src="./img/index/index_11_03.jpg" alt="" class="index_back11_03">  
-                    </div>
-                    <div class="index_hot_ppwrap hot_ppwrap03 d-flex flex-column text-center">
-                        <a class='index_hot_a' href="">
-                            <h6>好瘦瑜珈磚</h6>
-                        </a>
-                        <p>NT$. 1000</p>
+
+                        <div class="index_hot_ppwrap hot_ppwrap03 d-flex flex-column text-center">
+                            <a class='index_hot_a' href="">
+                                <h6>好瘦瑜珈磚</h6>
+                            </a>
+                            <p>NT$. 1000</p>
+                        </div>
                     </div>
                 </div>                
             </div>
@@ -1259,60 +1263,7 @@
             
         </div>
 
-    <!-- footer -->
-        <!-- <div class="footer">
-            <div class="container">
-                <div class="row mb-5 justify-content-center">
-                    <div class="col-8 d-flex p-0 flex-column flex-sm-column flex-md-row flex-lg-row">
-                        <div class="col-lg-4 col-md-4 col-sm-12 col-12" id="foot-info1">
-                            <h6 class="footer_big_title" style="color: #f2a200;">SERVICE</h6>
-                            <p class="footer_title_wrap">
-                                <a class="text-white" href="#">如何購買│How to Buy</a><br />
-                                <a class="text-white" href="#">產品特點│Identify</a><br />
-                                <a class="text-white" href="#">會員中心│Member</a><br />
-                                <a class="text-white" href="#">如何選擇│Find Your Soulmat</a>
-                            </p>
-                        </div>
-                        <div class="col-lg-4 col-md-4 col-sm-12 col-12 foot-info2">
-                            <h6 class="footer_big_title">INFORMATION</h6>
-                            <p class="footer_title_wrap">
-                                <a class="text-white" href="#">關於我們│About Us</a><br />
-                                <a class="text-white" href="#">產品資訊│Products</a><br />
-                                <a class="text-white" href="#">瑜珈提案│Projects</a><br />
-                                <a class="text-white" href="#">物流資訊│Delivery</a>
-                            </p>
-                        </div>
-                        <div class="col-lg-4 col-md-4 col-sm-12 col-12 foot-info3">
-                            <h6 class="footer_big_title">CONTACT</h6>
-                            <p class="footer_small_title">
-                                +886 012 34567<br />
-                                36yoga@gmail.com
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-lg-8 col-md-8 col-sm-8 col-10">
-                        <div class="d-flex justify-content-between">
-                            <div class="footer-logo">
-                                <img src="./SVG/logo.svg" alt="" />
-                            </div>
-                            <div class="d-flex align-items-center">
-                                <a href="#" class="social-icon"><img src="./SVG/fb.svg" alt="" /></a>
-                                <a href="#" class="social-icon"><img src="./SVG/line.svg" alt="" /></a>
-                                <a href="#" class="social-icon"><img src="./SVG/instagram.svg" alt="" /></a>
-                            </div>
-                        </div>
-                        <div class="dividerline"></div>
-                        <p class="text-center my-4 copyright">
-                            © 36 YOGA All Right<span class="mobile-display-none">
-                                Reserved.</span>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-
+    
     </div> 
 
 <?php include __DIR__ . '/parts/html-footer.php'; ?>
