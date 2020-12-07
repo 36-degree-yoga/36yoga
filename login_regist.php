1a73a7cd893da6137700d4cf2bc653d12cc92115
Butterfly-L <butterfly.liaw@gmail.com>
2020-12-07 15:05:37 +0800

    all rachel's

--- login_regist.php
+++ login_regist.php
@@ -38,10 +38,13 @@ $gotoURL = 'product_list.php';
                             <label for="password">
                                 <input type="password" class="form-control1" id="loginpassword" name="loginpassword" placeholder="&nbsp;密碼 Password">
                             </label>
+                            <div style="height:55px">
+
+                            </div>
                         </div>
                         <!-- 驗證碼 -->
                         <div class="form-group d-flex justify-content-between space-b36">
-                            <input type="text" class="open-sesame" name="checkword" size="10" maxlength="10" placeholder="&nbsp;驗證碼" />
+                            <input type="text" class="open-sesame" name="checkword" size="10" maxlength="10" id="login_code" placeholder="&nbsp;驗證碼" />
                             <div id="imgcode" src="" onclick="refresh_code()">
                                 <div class="randompic"></div>
                                 <!-- <img src="./img/index/random02.svg" alt="" class="randompic">
@@ -54,7 +57,9 @@ $gotoURL = 'product_list.php';
                             <a href="#" class=" ml-auto">忘記密碼？</a>
                         </div>
                         <div class="w-100 d-flex justify-content-center">
-                            <input type="submit" class="btn btn_f" value="登入"></div>
+                            <input type="submit" class="btn btn_f" value="登入" data-toggle="modal" data-target="#exampleModal">
+                            <span id="warn_login" style="color:red;"></span>
+                        </div>
                     </form>
                 </div>
 
@@ -67,13 +72,13 @@ $gotoURL = 'product_list.php';
                             </label>
 
                             <label for="password"><input type="password" class="form-control2 " id="regis_pw1" name="regis_pw1" placeholder="&nbsp;密碼 Password" required></label>
-                            <label for="password"><input type="password" class="form-control2" placeholder="&nbsp;二次輸入密碼 Password" required></label>
+                            <label for="password"><input type="password" class="form-control2" id="regis_pw2" placeholder="&nbsp;二次輸入密碼 Password" required></label>
                         </div>
                         <!-- 驗證碼 -->
-                        <!-- <div class="form-group d-flex justify-content-between space-b20">
-                                <input type="text" name="checkword" class="open-sesame" size="10" maxlength="10" placeholder="驗證碼" />
-                                <img id="imgcode" src="#" onclick="refresh_code()" />
-                            </div> -->
+                        <div class="form-group d-flex justify-content-between space-b20">
+                            <input type="text" name="checkword" id="regiscod2" class="open-sesame" size="10" maxlength="10" placeholder="驗證碼" />
+                            <div class="randompic"></div>
+                        </div>
 
                         <div class="form-group form-check form-regis space-b36 d-flex">
                             <input type="checkbox" class="form-check-input" id="registcheck">
@@ -81,10 +86,10 @@ $gotoURL = 'product_list.php';
                             <a href="#" class=" ml-auto">已有帳號？</a>
                         </div>
                         <div class="w-100 d-flex justify-content-center">
-                            <input type="submit" class="btn btn-primary btn_f" value="註冊">
+                            <input type="submit" class="btn btn-primary btn_f" value="註冊" data-toggle="modal" data-target="#exampleModal">
 
                         </div>
-                        <p id="regis_alert"></p>
+                        <!-- <p id="regis_alert" style="color:red;"></p> -->
                     </form>
 
                 </div>
@@ -96,6 +101,41 @@ $gotoURL = 'product_list.php';
 
 </div>
 
+<!-- 彈跳視窗登入↓↓ -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">
+                        <img src="./SVG/delete.svg" alt="" class="btn_delete">
+                    </span>
+                </button>
+            </div>
+            <form action="">
+                <div class="modal-body d-flex justify-content-center flex-column">
+
+                    <div class="edit_info d-flex align-items-center justify-content-center">
+
+
+                        <h6 id="login-alert-f" href="#" style="color: red;"></h6>
+                        <h5 id="login-alert-s" href="#" style="color: #135F39;"></h5>
+
+
+                    </div>
+
+
+
+                </div>
+            </form>
+            <div class="modal-footer">
+                <div class="space_30"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- 彈跳視窗↑↑ -->
+
 <?php include __DIR__ . '/parts/html-footer.php'; ?>
 <?php include __DIR__ . '/parts/script.php'; ?>
 <!-- js連結 -->
@@ -105,25 +145,70 @@ $gotoURL = 'product_list.php';
     // php部分-->登入
     const loginemail = $('#loginemail')
     const loginpassword = $('#loginpassword')
+    const logincode = $('#login_code')
 
 
     function checkForm() {
-        console.log(loginpassword + loginemail);
+        $('#login-alert-f').text('');
+        $('#login-alert-s').text('');
 
-        $.post('login-api.php', {
+        let isPass = true;
+        let error = ''
 
-            loginemail: loginemail.val(),
-            loginpassword: loginpassword.val()
-        }, function(data) {
-            if (data.success) {
-                console.log('hi');
-                location.href = '<?= $_SERVER['HTTP_REFERER'] ?>';
-            } else {
-                console.log('error');
+        if (loginemail.val()) {
+            if (!email_re.test(loginemail.val())) {
+                isPass = false;
+                error += '正確信箱格式';
+                // $('#login-alert').text('請填寫正確信箱格式  ');
             }
+        }
+
+        if (!loginemail.val()) {
+            isPass = false;
+            error += '註冊信箱  ';
+
+        }
+
+        if (!loginpassword.val()) {
+            isPass = false;
+            error += '密碼  ';
+
+        }
+
+        if (!logincode.val()) {
+            isPass = false;
+            error += '驗證碼  ';
+
+        }
+
+        if (error) {
+            isPass = false;
+            $('#login-alert-f').html("請填寫" + error);
+        }
 
 
-        }, 'json');
+
+        if (isPass) {
+            $.post('login-api.php', {
+
+
+                loginemail: loginemail.val(),
+                loginpassword: loginpassword.val()
+            }, function(data) {
+                if (data.success) {
+                    console.log('hi');
+                    $('#login-alert-s').text('登入成功');
+                    setTimeout(function() {
+                        location.href = '<?= $_SERVER['HTTP_REFERER'] ?>';
+                    }, 2000);
+                } else {
+                    console.log('error');
+                    $('#login-alert-f').text('登入失敗');
+                }
+
+
+            }, 'json');
+        }
     };
 
 
@@ -131,18 +216,54 @@ $gotoURL = 'product_list.php';
     const email_re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
     const regis_email = $('#regis_email');
     const regis_pw1 = $('#regis_pw1');
+    const regis_pw2 = $('#regis_pw2');
+    const regiscod2 = $('#regiscod2');
     let regis_alert = $('#regis_alert');
 
 
     function checkForm2() {
+        $('#login-alert-f').text('');
+        $('#login-alert-s').text('');
         let isPass = true;
+        let error = ''
+
 
         if (regis_email.val()) {
             if (!email_re.test(regis_email.val())) {
                 isPass = false;
-                regis_alert.text('請填寫正確的 email 格式!');
+                error += '正確 Email 格式  ';
+
             }
         }
+        if (!regis_email.val()) {
+
+            error += ' Email  ';
+
+        }
+        if (regis_pw1.val()) {
+            if (!regis_pw1.val() == regis_pw2.val()) {
+                isPass = false;
+                error += '相同密碼  ';
+            }
+        }
+
+        if (!regis_pw1.val()) {
+
+            error += '密碼  ';
+
+        }
+
+        if (!regiscod2.val()) {
+            isPass = false;
+            error += '驗證碼  ';
+
+        }
+
+        if (error) {
+            isPass = false;
+            $('#login-alert-f').html("請填寫" + error);
+        }
+
 
         if (isPass) {
             $.post('register-api.php', {
@@ -150,9 +271,13 @@ $gotoURL = 'product_list.php';
                 regis_pw1: regis_pw1.val()
             }, function(d) {
                 if (d.success) {
-                    console.log('hi registered');
-                    location.href = '<?= $gotoURL ?>';
+                    console.log('hi');
+                    $('#login-alert-s').text('註冊成功！');
+                    setTimeout(function() {
+                        location.href = '<?= $_SERVER['HTTP_REFERER'] ?>';
+                    }, 2000);
                 } else {
+                    $('#login-alert-f').text('註冊失敗！');
                     console.log('error fail to register');
                 }
             }, 'json');
