92a353dc9b3350580f43be7d08e4cb1dd1c9d2cb
Cheng-sih-rong <71741659+Cheng-sih-rong@users.noreply.github.com>
2020-12-08 13:54:11 +0800

    000

---
+++ about.php
@@ -0,0 +1,460 @@
+<?php include __DIR__ . '/parts/config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>36YOGA</title>
+<link rel="stylesheet" href="<?= WEB_ROOT ?>bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="<?= WEB_ROOT ?>fontawesome/css/all.css">
+<!-- <link rel="stylesheet" href="CSS/reset.css"> -->
+<link rel="stylesheet" href="CSS/about.css">
+<link rel="stylesheet" href="CSS/share.css">
+<link rel="stylesheet" href="<?= WEB_ROOT ?>CSS/nav.css">
+<link rel="stylesheet" href="<?= WEB_ROOT ?>CSS/footer.css" />
+
+
+
+<?php include __DIR__ . '/parts/nav.php'; ?>
+<!-- 貼上html -->
+
+<!-- about關於  電腦版 -->
+
+
+<div class="about container-fluid p-0">
+
+    <!-- 第一 -->
+
+    <div class="about_wrap about_section_1 d-flex  m-0 position-relative">
+
+        <div class="about_banner position-absolute col-12 d-flex justify-content-center p-0">
+            <div class="banner_img_wrap  p-0">
+                <img src="img/about/about_01.jpg" alt="">
+            </div>
+        </div>
+
+
+        <div class="yellow_back col-5 d-flex justify-content-end align-items-center position-relative">
+            <div class="scroll_down d-flex position-absolute align-items-center">
+                <p>Scroll Down</p>
+                <div class="mb-2"></div>
+            </div>
+
+            <h2 class="mr-5">關於我們</h2>
+        </div>
+
+
+        <div class="col-7 d-flex flex-column justify-content-center align-items-center p-0">
+            <div class="slogan d-flex flex-column">
+                <p>讓瑜珈的美好融入生活中</p>
+                <p>只需要一張瑜珈墊</p>
+            </div>
+            <div class="orange_line "></div>
+        </div>
+
+    </div>
+
+    <!-- 第二 -->
+    <div class="about_section2 d-flex flex-column">
+        <!-- logo -->
+        <div class="about_logo_wrap d-flex align-item-center">
+            <div class="logo_img col-5 d-flex justify-content-center ">
+                <img src="svg/about_logo.svg" alt="">
+            </div>
+
+            <div class="test  position-relative">
+                <div class="logo_title typing  ">
+                    <p>36度&emsp;<span>與人類共感的溫度</span></p>
+
+
+                    <div class="title_line position-absolute d-flex align-items-sm-center">
+                        <div class="O"></div>
+                        <div class="line"></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- 品牌精神 內容 -->
+        <div class="spirit col-lg-11 col-md-12 d-flex p-0 align-items-center">
+            <img src="img/about/about_02.jpg" alt="">
+
+            <div class="letter">
+                <h3>品牌精神</h3>
+                <br>
+                <br>
+                <p>大多數人不知道該如何挑選好的瑜珈商品，對於材質也較沒有概念、更不知道怎麼買適合的輔具，因此對於瑜珈有了距離感。</p>
+
+
+                <br>
+                <p>我們希望讓瑜珈更有溫度、更生活化，讓每個人都能夠輕鬆接觸瑜珈，找到適合他們的瑜珈產品，讓瑜珈的美好融入日常！</p>
+            </div>
+        </div>
+
+    </div>
+
+
+    <!-- 第三區 -->
+    <div class="about_srction3 justify-content-center">
+
+        <div class="page col-10 d-flex flex-column justify-content-center">
+            <!-- 標題 -->
+            <div class="point d-flex justify-content-center align-items-center">
+                <div class="point_line_l"></div>
+                <h2 class="mx-5 co-5">品牌特點</h2>
+                <div class="point_line_r"></div>
+            </div>
+
+            <div class="d-flex p-0 align-items-center justify-content-center">
+                <!-- 內容 -->
+                <div class="point_letter col-4">
+                    <p>研發創新、優質的瑜珈墊，比起市面上的更加長、更加寬一些的尺寸，選擇並創造最適合您的瑜珈墊，強化墊子的功能，防滑穩定各種瑜珈姿勢，讓練習更自在寫意。
+                    </p>
+                    <br>
+                    <p>
+                        由純天然橡膠和環保PU製成，使用高科技熱能貼合製成，可生物分解，從材質的篩選到嚴謹製程，友愛地球和人體的健康。</p>
+                </div>
+                <!-- icon -->
+                <div class="point_icon col-5">
+                    <div class="row justify-content-end">
+                        <div class=" identify_wrap mx-5 d-flex flex-column align-items-center">
+                            <img class="identify_box" src="SVG/identify_01.svg" alt="">
+                            <h6>環保無毒</h6>
+                        </div>
+                        <div class=" identify_wrap mx-5 d-flex flex-column align-items-center">
+                            <img class="identify_box" src="SVG/identify_02.svg" alt="">
+                            <h6>台灣製造</h6>
+                        </div>
+                        <div class="  identify_wrap mx-5 d-flex flex-column align-items-center">
+                            <img class="identify_box" src="SVG/identify_03.svg" alt="">
+                            <h6>專利防滑</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="point_img d-flex col-12 justify-content-center ">
+                <img class="" src="img/about/about_03_01.jpg" alt="">
+                <img class="" src="img/about/about_03_02.jpg" alt="">
+                <img class="" src="img/about/about_03_03.jpg" alt="">
+            </div>
+        </div>
+    </div>
+
+
+    <!-- 第四區 -->
+    <div class="about_section4 d-flex justify-content-center">
+
+        <div class="content_4 col-10 align-items-center">
+            <h2 class="col-5">願景目標</h2>
+
+            <div class="col-5">
+                一個企業想對自己的品牌有所期許，或對一個喜愛的品牌有所期待， 必須先看到一個品牌除了產品本身之外，為生活帶來更高價值的東西。
+                <br>
+                <br>
+                無論這個更高價值的東西是一般人講的附加價值也好，或是一個觀念、品味、生活態度也罷，我們只想去挖掘更多熱愛生活的人，一起做喜歡的開心事。
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
+
+
+
+<!-- ___________________手機版本__________________ -->
+
+<div class="m_about container-fluid p-0">
+
+    <!-- 第一 -->
+
+    <div class="about_wrap about_section_1 d-flex justify-content-end  m-0 position-relative">
+
+        <div class="m_about_banner position-absolute col-12 d-flex justify-content-center p-0">
+            <div class="banner_img_wrap  p-0">
+                <img src="img/about/about_01.jpg" alt="">
+            </div>
+        </div>
+
+
+        <div class="m_scroll col-1 d-flex flex-column  justify-content-center align-items-center p-0 position-relative">
+            <div class="scroll_down d-flex  align-items-center">
+                <p class="">Scroll Down</p>
+                <div class="mb-2 col"></div>
+            </div>
+        </div>
+
+
+        <div class="yellow_back col-10 d-flex flex-column justify-content-end position-relative p-0">
+
+            <div class="m_title_wrap d-flex flex-column  w-75">
+
+                <h2 class="align-self-end">關於我們</h2>
+
+                <div class=" d-flex p-0 flex-column  justify-content-center align-items-end p-0">
+                    <div class="slogan d-flex flex-column align-items-end">
+                        <p>讓瑜珈的美好融入生活中，</p>
+                        <p>只需要一張瑜珈墊</p>
+                    </div>
+                    <div class="orange_line"></div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- 第二 -->
+    <div class="about_section2">
+
+        <!-- logo -->
+        <div class="about_logo_wrap align-item-center">
+
+
+            <div class="test d-flex position-relative">
+                <div class="logo_title typing  ">
+                    <p>36度&emsp;<span>與人類共感的溫度</span></p>
+
+
+                    <div class="title_line  position-absolute d-flex align-items-center">
+                        <div class="O"></div>
+                        <div class="line"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="logo_img d-flex justify-content-center mt-5">
+                <img class="col-5" src="svg/about/about_logo.svg" alt="">
+            </div>
+
+        </div>
+        <!-- 品牌精神 內容 -->
+        <div class="d-flex justify-content-end">
+            <div class="spirit col-10 d-flex p-0 align-items-end">
+
+                <div class="letter">
+                    <h3>品牌精神</h3>
+                    <br>
+                    <p>大多數人不知道該如何挑選好的瑜珈商品，對於材質也較沒有概念、更不知道怎麼買適合的輔具，因此對於瑜珈有了距離感。</p>
+                    <p>我們希望讓瑜珈更有溫度、更生活化，讓每個人都能夠輕鬆接觸瑜珈，找到適合他們的瑜珈產品，讓瑜珈的美好融入日常！</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <!-- 第三區 -->
+    <div class="about_srction3 justify-content-center">
+
+        <div class="page col-11 d-flex flex-column justify-content-center">
+            <!-- 標題 -->
+            <div class="point d-flex justify-content-center align-items-center">
+                <div class="point_line_l col"></div>
+                <h2 class="mx-3">品牌特點</h2>
+                <div class="col point_line_r"></div>
+            </div>
+
+
+            <!-- 內容 -->
+            <div class="point_letter ">
+                <p>研發創新、優質的瑜珈墊，比起市面上的更加長、更加寬一些的尺寸，選擇並創造最適合您的瑜珈墊，強化墊子的功能，防滑穩定各種瑜珈姿勢，讓練習更自在寫意。
+                </p>
+
+
+                <!-- icon -->
+                <div class="point_icon">
+                    <div class="row justify-content-around flex-nowrap align-items-end my-4">
+                        <div class=" identify_wrap mx-3 d-flex flex-column align-items-center">
+                            <img class="identify_box" src="SVG/identify_01.svg" alt="">
+                            <p>環保無毒</p>
+                        </div>
+                        <div class=" identify_wrap mx-3 d-flex flex-column align-items-center">
+                            <img class="identify_box" src="SVG/identify_02.svg" alt="">
+                            <p>台灣製造</p>
+                        </div>
+                        <div class="  identify_wrap mx-3 d-flex flex-column align-items-center">
+                            <img class="identify_box" src="SVG/identify_03.svg" alt="">
+                            <p>專利防滑</p>
+                        </div>
+
+                    </div>
+                    <p>
+                        由純天然橡膠和環保PU製成，使用高科技熱能貼合製成，可生物分解，從材質的篩選到嚴謹製程，友愛地球和人體的健康。</p>
+                </div>
+
+            </div>
+            <div class="point_img d-flex col-12 justify-content-center ">
+                <img class="" src="img/about/about_03_01.jpg" alt="">
+
+            </div>
+        </div>
+    </div>
+
+
+    <!-- 第四區 -->
+    <div class="about_section4 d-flex justify-content-start">
+
+        <div class="content_4  align-items-center justify-content-start">
+            <h2 class="">願景目標</h2>
+
+            <div class="col-7">
+                一個企業想對自己的品牌有所期許，或對一個喜愛的品牌有所期待， 必須先看到一個品牌除了產品本身之外，為生活帶來更高價值的東西。
+                <br>
+                <br>
+                無論這個更高價值的東西是一般人講的附加價值也好，或是一個觀念、品味、生活態度也罷，我們只想去挖掘更多熱愛生活的人，一起做喜歡的開心事。
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
+
+
+　
+
+<!--___________________↑ 手機板↑_________________ -->
+
+
+
+
+
+
+<!-------------------------- Script ----------------------------------->
+<?php include __DIR__ . '/parts/html-footer.php'; ?>
+<?php include __DIR__ . '/parts/script.php'; ?>
+
+<script>
+    //nav icon hover 效果//
+    $('.nav-icon').hover(function() {
+        $(this).find('.nav-icon-line').css('fill', '#DB5C00').css('transition', '.5s');
+        $(this).find('.nav-icon-stroke').css('stroke', '#DB5C00').css('transition', '.5s');
+
+    })
+
+    $('.nav-icon').click(function() {
+        $(this).find('.nav-icon-line').css('fill', '#DB5C00').css('transition', '.5s');
+        $(this).find('.nav-icon-stroke').css('stroke', '#DB5C00').css('transition', '.5s');
+
+    })
+
+    $('.nav-icon').mouseleave(function() {
+        $('.nav-icon-line').css('fill', '#004a13').css('transition', '.5s');
+        $('.nav-icon-stroke').css('stroke', '#004a13').css('transition', '.5s');
+    })
+
+    $('.nav-logo').hover(function() {
+        $(this).find('path').css('fill', '#DB5C00').css('transition', '.5s');
+
+    })
+
+    $('.nav-logo').click(function() {
+        $(this).find('path').css('fill', '#DB5C00').css('transition', '.5s');
+
+    })
+
+
+    $('.nav-logo').mouseleave(function() {
+        $('.nav-logo').find('path').css('fill', '#004a13').css('transition', '.5s');
+
+    })
+
+
+    // navbar捲動消失//
+    let scrollLast = 0;
+
+    $(document).ready(function() {
+        let device = $(window).width();
+        if (device < 992) {
+            $('.nav-desktop').removeClass('d-flex').css('display', 'none');
+            $('.nav-mobile').addClass('d-flex').css('display', 'inline-block');
+
+
+        } else {
+            $('.nav-desktop').css('display', 'inline-block').addClass('d-flex');
+            $('.nav-mobile').removeClass('d-flex').css('display', 'none');
+
+            $(window).scroll(function() {
+
+                let scrollNow = $(this).scrollTop();
+
+                if (scrollLast < scrollNow) {
+                    $('.nav-desktop').addClass('hide');
+                } else {
+                    $('.nav-desktop').removeClass('hide');
+                }
+
+                scrollLast = scrollNow;
+
+            })
+
+        }
+    });
+    ///判斷手機版顯示Navbar
+    $(window).resize(function() {
+
+        let device = $(window).width();
+        if (device < 992) {
+            $('.nav-desktop').removeClass('d-flex').css('display', 'none');
+            $('.nav-mobile').addClass('d-flex').css('display', 'none');
+            //inline-block
+
+
+        } else {
+            $('.nav-desktop').css('display', 'inline-block').addClass('d-flex');
+            $('.nav-mobile').removeClass('d-flex').css('display', 'none');
+            $('.nav-search-area').removeClass('show');
+            $(window).scroll(function() {
+
+                let scrollNow = $(this).scrollTop();
+
+                if (scrollLast < scrollNow) {
+                    $('nav').addClass('hide');
+                } else {
+                    $('nav').removeClass('hide');
+                }
+
+                scrollLast = scrollNow;
+
+            })
+
+        }
+
+    });
+
+    ///手機版搜尋點擊顯示   切換其他地方會隱藏
+    $('.nav-search').on('click', function(e) {
+        e.stopPropagation();
+        $('.nav-search-area').toggleClass('show');
+        $('.nav-icon-mobile').toggleClass('hide');
+        $('.nav-logo').toggleClass('hide');
+    })
+
+
+
+    $(document).click(function(e) {
+        var target = $(e.target);
+        if (target.closest("nav").length != 0) return;
+        $('.nav-search-area.show').removeClass('show');
+        $('.nav-icon-mobile.hide').removeClass('hide');
+        $('.nav-logo.hide').removeClass('hide');
+    })
+
+    //https://www.jianshu.com/p/42d9bc59e25a
+
+
+    //
+    $('.menu-icon').on('click', function() {
+        $('.nav-mobile-side').css('right', '0%').css('display', 'flex').css('opacity', '1');
+        $('.side-top').css('display', 'block');
+
+    })
+
+    $('.close-btn').on('click', function() {
+        $('.nav-mobile-side').css('right', '100%').css('display', 'none').css('opacity', '0');
+        $('.side-top').css('display', 'none');
+
+    });
+</script>
+
+<?php include __DIR__ . '/parts/html-end.php'; ?>
